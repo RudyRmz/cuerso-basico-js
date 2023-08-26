d9caa44b8a5c675c43e2ffc802e915fd106337dc
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2023-08-26 17:25:04 -0600

    firts commit

---
+++ variables.js
@@ -0,0 +1,20 @@
+let nombre = 'Juan';
+
+//declarar
+let edad;
+
+edad //no definido por lo que es undefined
+//inicializar
+edad = 30;
+
+edad //ya definido por lo que es 30 number
+
+const elementos = ['computadora', 'celular']; //String
+
+const persona = {
+    nombre: 'Diego',
+    edad: 30
+} //Contiene un string y un numero
+
+
+persona; //Objeto
